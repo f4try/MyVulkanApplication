eddcd5f7fb562a62bda81f4a4bd40a4903e70c0c
zz <zongzh.hd@163.com>
2021-06-10 13:00:41 +0800

    add build.bat

---
+++ build.bat
@@ -0,0 +1,21 @@
+cd %~dp0
+IF NOT EXIST shaders mkdir shaders
+%VULKAN_SDK%\Bin\glslc.exe shader.vert -o shaders\vert.spv
+%VULKAN_SDK%\Bin\glslc.exe shader.frag -o shaders\frag.spv
+
+@REM Build for Visual Studio compiler. Run your copy of amd64/vcvars32.bat to setup 64-bit command-line compiler.
+
+@set INCLUDES=/I %VULKAN_SDK%\include
+@set SOURCES=main.cpp
+@set LIBS=/libpath:%VULKAN_SDK%\lib glfw3.lib opengl32.lib gdi32.lib shell32.lib vulkan-1.lib user32.lib
+@set OUT_EXE=MyVulkanApplication
+
+@set OUT_DIR=Debug
+mkdir %OUT_DIR%
+cl /std:c++17 /EHsc /nologo /Zi /MD %INCLUDES% %SOURCES% /Fe%OUT_DIR%/%OUT_EXE%.exe /Fo%OUT_DIR%/ /link %LIBS%
+%~dp0/Debug/MyVulkanApplication.exe
+
+@REM @set OUT_DIR=Release
+@REM mkdir %OUT_DIR%
+@REM cl /nologo /Zi /MD /Ox /Oi %INCLUDES% %SOURCES% /Fe%OUT_DIR%/%OUT_EXE%.exe /Fo%OUT_DIR%/ /link %LIBS%
+
